57727e64e2e8c61256c92c319a782e4553af230c
ElTaitaImbabura <vihunape@gmail.com>
2026-04-22 22:47:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-df = pd.read_csv("https://dev-econ.vercel.app/05_unified_services_2018_2024.csv")
+df = pd.read_csv("https://dev-econ-git-main-johns-projects-44e072b9.app/05_unified_services_2018_2024.csv")
 
 df = df.dropna(subset=["ICT_Exp-Imp", "Cloud_Revenue_pC", "Year"]).copy()
 df["Year"] = df["Year"].astype(int)
