da00779222d627f4e744bc44ea30d2b0bece5aac
ElTaitaImbabura <vihunape@gmail.com>
2026-04-22 23:49:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+from pathlib import Path
+import pandas as pd
 import dash
 from dash import html
 
+BASE_DIR = Path(__file__).resolve().parent
+CSV_PATH = BASE_DIR / "05_unified_services_2018_2024.csv"
+
+print("Trying to load:", CSV_PATH)
+
+df = pd.read_csv(CSV_PATH)
+
+print("CSV loaded")
+print(df.head())
+
 dash_app = dash.Dash(__name__)
-dash_app.layout = html.Div("Hello from Dash on Vercel")
+dash_app.layout = html.Div("CSV LOADED SUCCESSFULLY")
 
 app = dash_app.server
-
-if __name__ == "__main__":
-    dash_app.run(debug=True)
